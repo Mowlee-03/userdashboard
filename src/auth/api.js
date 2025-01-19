48bafcc0ebd7883f8b77ed08349ade55abafdd10
Mowlee-03 <mowleemm@gmail.com>
2025-01-19 22:11:48 +0530

    changes

--- src/auth/api.js
+++ src/auth/api.js
@@ -1,6 +1,6 @@
-const BASE_URL=process.env.REACT_APP_BACKEND_URL
+const API_BASE_URL=process.env.REACT_APP_API_BASE_URL
 
 
 
-export const LOGIN_USER=`${BASE_URL}/api/user/login`
-export const SIGNUP_USER=`${BASE_URL}/api/user/signup`
+export const LOGIN_USER=`${API_BASE_URL}/api/user/login`
+export const SIGNUP_USER=`${API_BASE_URL}/api/user/signup`
